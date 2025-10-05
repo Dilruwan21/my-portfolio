de2c624a98d96ae42f70da65341ae6c803e61ed5
Anuradha Dilruwan <136330227+Dilruwan21@users.noreply.github.com>
2025-10-05 08:37:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { SiTypescript, SiMongodb, SiFirebase, SiTailwindcss, SiNextdotjs, SiGrap
 import { BsAwardFill } from "react-icons/bs";
 
 // Import profile image and resume
-import profilePic from './assect/_me.jpg';
+import profilePic from 'XAOD9748.JPEG';
 import resumeFile from './assect/Resume_Anuradha.pdf';
 
 // Import project images
